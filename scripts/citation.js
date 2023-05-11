9219e860f4ccec5e9f6005969dd8405a5c9dde59
charlazz <123973568+charlazz@users.noreply.github.com>
2023-05-11 09:55:28 +0200

    design css

--- scripts/citation.js
+++ scripts/citation.js
@@ -9,7 +9,12 @@ const citations = [
     "“La motivation vous fait démarrer, l’habitude vous fait continuer.” - Jim Ryun",
     "“Le succès n’est pas la clé du bonheur. Le bonheur est la clé du succès. Si vous aimez ce que vous faites, vous réussirez.” - Albert Schweitzer",
     "“Le temps est un bien plus précieux que l’argent. Vous pouvez obtenir plus d’argent, mais vous ne pouvez pas obtenir plus de temps.” - Jim Rohn",
-    "“Faites de chaque jour votre chef-d’œuvre.” - John Wooden"
+    "“Faites de chaque jour votre chef-d’œuvre.” - John Wooden",
+    "“La seule limite à nos réalisations de demain sont nos doutes d'aujourd'hui.” - Franklin D. Roosevelt",
+    "“Si vous avez le courage de commencer, vous avez le courage de réussir.” - David Viscott",
+    "“Le plus dur dans la réussite, c'est de se décider à agir, le reste n'est que de la ténacité.” - Amelia Earhart",
+    "“Le talent est un cadeau, mais le travail acharné est un choix.” - Kobe Bryant",
+
 
 ]
 
@@ -44,36 +49,58 @@ const citations = [
     const head = document.querySelector('head');
     const style = document.createElement('style');
     style.innerHTML = `
-        @import url('https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap');
-    
+    @import url('https://fonts.googleapis.com/css2?family=Asap:wght@200&display=swap');
+    @import url("https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=Pacifico&family=Pangolin&display=swap");
+  
+
         .CamilleClaraSteve {
-            position: fixed;
-            bottom: 15px;
-            right: 0px;
-            z-index: 4;
-            color: black;
-            clip-path: circle(8% at 91.5% 30%);
-            transition: all 0.5s ease-in-out;
-            border-radius: 10px;
-            padding: 10px;
-            width: 250px;
-            height: 100px;
-            text-align: center;
-            background: rgb(255,255,207);
-            background: radial-gradient(circle, rgba(255,255,207,0.7036064425770308) 13%, rgba(247,197,126,1) 84%);
+            /* From https://css.glass */
+                background: rgba(156, 228, 247, 0.79);
+                border-radius: 16px;
+                box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+                backdrop-filter: blur(4px);
+                -webkit-backdrop-filter: blur(4px);
+                border: 1px solid rgba(156, 228, 247, 0.3);
+                            
+        }
+
+        
+            .CamilleClaraSteve {
+                
+                backface-visibility: hidden;
+                position: fixed;
+                bottom: 15px;
+                right: 0px;
+                z-index: 4;
+                color: black;
+                clip-path: circle(8% at 91.5% 15%);
+                transition: all 0.5s ease-in-out;
+                border-radius: 10px;
+                padding: 10px;
+                width: 250px;
+                height: 100px;
+                text-align: center;
+                box-shadow: 0 6px 7px -1px #ffffff;
+               
+                
+               
+                
         }
         .CamilleClaraSteve:hover {
+            
             clip-path: circle(75%);
         }
         .CamilleClaraSteve span {
             float: right;
             position: relative;
             margin-right: 2%;
+            
         }
         .CamilleClaraSteve h6 {
             padding-top: 20px;
             font-size: 14px;
-            font-family: 'Gochi Hand', cursive;
+            font-family: 'asap', cursive;
+            
         }
         `;
     head.appendChild(style);
