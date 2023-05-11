ff8270a97b7c22ff83191485a5590b6a386e6891
Camille Hebert <camille.hebert22@gmail.com>
2023-05-11 10:33:27 +0200

    ajout CSS clara + suppr fichiers inutiles

--- scripts/citation.js
+++ scripts/citation.js
@@ -1,21 +1,4 @@
-const citations = [
-
-    "“La meilleure façon de prédire votre avenir est de le créer.” - Abraham Lincoln",
-    "“Le succès est la somme de petits efforts répétés jour après jour.” - Robert Collier",
-    "“Le travail acharné bat le talent quand le talent ne travaille pas dur.” - Tim Notke",
-    "“Ne laissez jamais personne vous dire que vous ne pouvez pas faire quelque chose. Si vous avez un rêve, vous devez le protéger.” - Chris Gardner",
-    "“Lorsque vous voulez réussir aussi fort que vous voulez respirer, alors vous réussirez.” - Eric Thomas",
-    "“Si vous voulez vivre une vie heureuse, attachez-la à un but, pas à des personnes ou à des choses.” - Albert Einstein",
-    "“La motivation vous fait démarrer, l’habitude vous fait continuer.” - Jim Ryun",
-    "“Le succès n’est pas la clé du bonheur. Le bonheur est la clé du succès. Si vous aimez ce que vous faites, vous réussirez.” - Albert Schweitzer",
-    "“Le temps est un bien plus précieux que l’argent. Vous pouvez obtenir plus d’argent, mais vous ne pouvez pas obtenir plus de temps.” - Jim Rohn",
-    "“Faites de chaque jour votre chef-d’œuvre.” - John Wooden"
-
-]
-
-// setInterval(() => {
-    
-    const html = document.querySelector("body");
+const html = document.querySelector("body");
     
     
     if(html){
@@ -52,38 +35,59 @@ const citations = [
     const head = document.querySelector('head');
     const style = document.createElement('style');
     style.innerHTML = `
-        @import url('https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap');
-    
+    @import url('https://fonts.googleapis.com/css2?family=Asap:wght@200&display=swap');
+  
+
         .CamilleClaraSteve {
-            position: fixed;
-            bottom: 15px;
-            right: 0px;
-            z-index: 4;
-            color: black;
-            clip-path: circle(8% at 91.5% 30%);
-            transition: all 0.5s ease-in-out;
-            border-radius: 10px;
-            padding: 10px;
-            width: 250px;
-            height: 100px;
-            text-align: center;
-            background: rgb(255,255,207);
-            background: radial-gradient(circle, rgba(255,255,207,0.7036064425770308) 13%, rgba(247,197,126,1) 84%);
+            /* From https://css.glass */
+                background: rgba(156, 228, 247, 0.79);
+                border-radius: 16px;
+                box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+                backdrop-filter: blur(4px);
+                -webkit-backdrop-filter: blur(4px);
+                border: 1px solid rgba(156, 228, 247, 0.3);
+                            
+        }
+
+        
+            .CamilleClaraSteve {
+                
+                backface-visibility: hidden;
+                position: fixed;
+                bottom: 15px;
+                right: 0px;
+                z-index: 1000;
+                color: black;
+                clip-path: circle(8% at 91% 22%);
+                transition: all 0.5s ease-in-out;
+                border-radius: 10px;
+                padding: 10px;
+                width: 250px;
+                height: 100px;
+                text-align: center;
+                box-shadow: 0 6px 7px -1px #ffffff;
+               
+                
+               
+                
         }
         .CamilleClaraSteve:hover {
+            
             clip-path: circle(75%);
         }
         .CamilleClaraSteve span {
             float: right;
             position: relative;
             margin-right: 2%;
+            
         }
         .CamilleClaraSteve h6 {
-            padding-top: 20px;
-            font-size: 14px;
-            font-family: 'Gochi Hand', cursive;
+            padding-top: 10px;
+            font-size: 12px;
+            font-family: 'asap', cursive;
+            
         }
         `;
     head.appendChild(style);
-// }, 1000);
+
 
